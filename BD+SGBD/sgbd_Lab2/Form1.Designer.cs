6c1dc7564d55b062366b4d9563d1ec27d3d91068
DianaElena99 <40459272+DianaElena99@users.noreply.github.com>
2020-04-19 20:27:07 +0300

    Add files via upload

---
+++ BD+SGBD/sgbd_Lab2/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿using System;
+
+namespace SGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabelUniv = new System.Windows.Forms.DataGridView();
+            this.parentTable = new System.Windows.Forms.Label();
+            this.childTable = new System.Windows.Forms.Label();
+            this.TabelProfi = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelUniv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelProfi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TabelUniv
+            // 
+            this.TabelUniv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TabelUniv.Location = new System.Drawing.Point(36, 56);
+            this.TabelUniv.Margin = new System.Windows.Forms.Padding(5);
+            this.TabelUniv.Name = "TabelUniv";
+            this.TabelUniv.RowTemplate.Height = 28;
+            this.TabelUniv.Size = new System.Drawing.Size(462, 258);
+            this.TabelUniv.TabIndex = 0;
+            this.TabelUniv.SelectionChanged += new System.EventHandler(this.TabelUniv_SelectionChanged);
+            // 
+            // parentTable
+            // 
+            this.parentTable.AutoSize = true;
+            this.parentTable.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parentTable.Location = new System.Drawing.Point(41, 20);
+            this.parentTable.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.parentTable.Name = "parentTable";
+            this.parentTable.Size = new System.Drawing.Size(143, 31);
+            this.parentTable.TabIndex = 1;
+            this.parentTable.Text = "Universitati";
+            // 
+            // childTable
+            // 
+            this.childTable.AutoSize = true;
+            this.childTable.Location = new System.Drawing.Point(612, 9);
+            this.childTable.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.childTable.Name = "childTable";
+            this.childTable.Size = new System.Drawing.Size(116, 31);
+            this.childTable.TabIndex = 2;
+            this.childTable.Text = "Profesori";
+            // 
+            // TabelProfi
+            // 
+            this.TabelProfi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TabelProfi.Location = new System.Drawing.Point(558, 56);
+            this.TabelProfi.Name = "TabelProfi";
+            this.TabelProfi.RowTemplate.Height = 28;
+            this.TabelProfi.Size = new System.Drawing.Size(488, 312);
+            this.TabelProfi.TabIndex = 3;
+            this.TabelProfi.SelectionChanged += new System.EventHandler(this.TabelProfi_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 81);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Adauga ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(821, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sterge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(821, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 69);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Modifica";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 354);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 38);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(256, 415);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 38);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(256, 469);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(262, 38);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nume si prenume";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(99, 418);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Universitate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 469);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 31);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Nr cursuri/semestru";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Asistent",
+            "Lector",
+            "Conferentiar",
+            "Profesor"});
+            this.comboBox1.Location = new System.Drawing.Point(36, 543);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 39);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Text = "Grad";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ComputerScience",
+            "Mathematics",
+            "Literature",
+            "Philosophy",
+            "Physics&Chemistry",
+            "Medicine",
+            "Engineering",
+            "Politics",
+            "History"});
+            this.comboBox2.Location = new System.Drawing.Point(256, 543);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(262, 39);
+            this.comboBox2.TabIndex = 17;
+            this.comboBox2.Text = "Specializare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 669);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TabelProfi);
+            this.Controls.Add(this.childTable);
+            this.Controls.Add(this.parentTable);
+            this.Controls.Add(this.TabelUniv);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TabelUniv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelProfi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TabelUniv;
+        private System.Windows.Forms.Label parentTable;
+        private System.Windows.Forms.Label childTable;
+        private System.Windows.Forms.DataGridView TabelProfi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ BD+SGBD/sgbd_Lab2/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SGBD
+{
+    public partial class Form1 : Form
+    {
+        private static int IDProf = 1;
+
+        public string connectionStr = ConfigurationManager.AppSettings["connectionString"];
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter adapterUni = new SqlDataAdapter();
+        SqlDataAdapter adapterProf = new SqlDataAdapter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void initIDCounter()
+        {
+            using(SqlConnection con = new SqlConnection(connectionStr))
+            {
+                con.Open();
+                SqlCommand comanda = new SqlCommand("SELECT max(codprof) FROM PROFESORI", con);
+                var R = comanda.ExecuteReader();
+                while (R.Read())
+                {
+                    int N = R.GetInt32(0);
+                    IDProf = N;
+                }
+            }
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                con.Open();
+                ds.Clear();
+
+                adapterUni.SelectCommand = new SqlCommand("Select * from Universitate", con);
+                adapterUni.Fill(ds, "TabelUniv");
+
+                adapterProf.SelectCommand = new SqlCommand("Select * from Profesori", con);
+                adapterProf.Fill(ds, "TabelProfi");
+                   
+                TabelUniv.DataSource = ds.Tables["TabelUniv"];
+                TabelProfi.DataSource = ds.Tables["TabelProfi"];
+
+                TabelUniv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            }
+        }
+
+        //ADAUGARE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initIDCounter();
+            string numePr = textBox1.Text;
+            int UnivId = 0;
+            try
+            {
+                UnivId = int.Parse(textBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ID universitate trebuie sa fie numar");
+            }
+            int nrCursuri = int.Parse(textBox4.Text);
+            if (nrCursuri < 1 || nrCursuri > 3)
+            {
+                MessageBox.Show("Nr invalid de cursuri");
+                return;
+            }
+            string grad = comboBox1.SelectedItem.ToString();
+            string specializ = comboBox2.SelectedItem.ToString();
+
+            if (UnivId != 0)
+            {
+                DialogResult res = MessageBox.Show("Doriti sa adaugati profesorul cu nume : " + numePr +
+                " uniID : " + UnivId + " nrCursuri:" + nrCursuri + " specializare:" + specializ + "?", "", MessageBoxButtons.OKCancel);
+                if (res == DialogResult.OK)
+                {
+                    using (SqlConnection con = new SqlConnection(connectionStr))
+                    {
+                        try
+                        {
+                            con.Open();
+                            IDProf += 2;
+                            adapterProf.InsertCommand = new SqlCommand("Insert into Profesori values (@IdProf, @Specializare, @UnivId, @Grad, @NrCursuri, @Nume)", con);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@IdProf", IDProf);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@Nume", numePr);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@Specializare", specializ);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@NrCursuri", nrCursuri);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@Grad", grad);
+                            adapterProf.InsertCommand.Parameters.AddWithValue("@UnivId", UnivId);
+                            int affectedRows = adapterProf.InsertCommand.ExecuteNonQuery();
+                            MessageBox.Show(affectedRows.ToString() + " row(s) affected");
+                            ds.Tables["TabelProfi"].Clear();
+                            adapterProf.SelectCommand = new SqlCommand("Select * from Profesori", con);
+                            adapterProf.Fill(ds, "TabelProfi");
+                            TabelProfi.DataSource = ds.Tables["TabelProfi"];
+                        }catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                       
+                    }
+                }
+            }
+
+        }
+
+        //STERGERE - Se sterge profesorul selectat
+        // DELETE FROM PROFESORI WHERE codProf = ...
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { 
+            var row = TabelProfi.SelectedRows[0];
+            int idProf = int.Parse( TabelProfi.Rows[row.Index].Cells[0].Value.ToString());
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                con.Open();
+                adapterProf = new SqlDataAdapter();
+                string comanda = "DELETE FROM PROFESORI WHERE codProf = @idProf";
+                adapterProf.DeleteCommand = new SqlCommand(comanda, con);
+                adapterProf.DeleteCommand.Parameters.AddWithValue("@idProf", idProf);
+                int n = adapterProf.DeleteCommand.ExecuteNonQuery();
+                MessageBox.Show(n.ToString() + " row(s) affected");
+
+                ds.Tables["TabelProfi"].Clear();
+                adapterProf.SelectCommand = new SqlCommand("SELECT * FROM PROFESORI", con);
+                adapterProf.Fill(ds, "TabelProfi");
+                TabelProfi.DataSource = ds.Tables["TabelProfi"];
+                con.Close();
+            }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //MODIFICARE
+        // UPDATE PROFESORI SET nume = @nume, grad = @grad, 
+        //                      nrCursuri = @nrCursuri, specializare = @specializare 
+        // WHERE codProf = @codProf  
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try { 
+            using (SqlConnection con = new SqlConnection(connectionStr)) {
+                con.Open();
+                string nume = textBox1.Text;
+                string grad = comboBox1.Text;
+                string specializare = comboBox2.Text;
+                int nrCursuri = int.Parse(textBox4.Text);
+                int uniId = int.Parse(textBox3.Text);
+
+                int codProf = int.Parse(TabelProfi.SelectedRows[0].Cells[0].Value.ToString());
+
+                adapterProf = new SqlDataAdapter();
+
+                string cmd = "UPDATE Profesori SET nume = @nume,grad = @grad," +
+                    "nrCoursesPerSemester = @nrCursuri, field = @specializare " +
+                    "WHERE codProf = @codProf";
+
+                adapterProf.UpdateCommand = new SqlCommand(cmd, con);
+                adapterProf.UpdateCommand.Parameters.AddWithValue("@nume", nume);
+                adapterProf.UpdateCommand.Parameters.AddWithValue("@nrCursuri", nrCursuri);
+                adapterProf.UpdateCommand.Parameters.AddWithValue("@grad", grad);
+                adapterProf.UpdateCommand.Parameters.AddWithValue("@specializare", specializare);
+                adapterProf.UpdateCommand.Parameters.AddWithValue("@codProf", codProf);
+
+                int n = adapterProf.UpdateCommand.ExecuteNonQuery();
+                MessageBox.Show(n.ToString() + " row(s) affected");
+
+                ds.Tables["TabelProfi"].Clear();
+                adapterProf.SelectCommand = new SqlCommand("SELECT * FROM PROFESORI", con);
+                adapterProf.Fill(ds, "TabelProfi");
+                TabelProfi.DataSource = ds.Tables["TabelProfi"];
+                con.Close();
+            }
+            }catch(Exception xe)
+            {
+                MessageBox.Show(xe.Message);
+            }
+
+        }
+
+        private void TabelProfi_SelectionChanged(object s, EventArgs E)
+        {
+            if (TabelProfi.SelectedRows.Count == 1)
+            {
+                var idx = TabelProfi.SelectedRows[0];
+                int profId = int.Parse(TabelProfi.Rows[idx.Index].Cells[0].Value.ToString());
+                textBox1.Text = profId.ToString();
+
+                string nume = TabelProfi.Rows[idx.Index].Cells[5].Value.ToString();
+                textBox1.Text = nume;
+                comboBox2.Text = TabelProfi.Rows[idx.Index].Cells[1].Value.ToString();
+                comboBox1.Text = TabelProfi.Rows[idx.Index].Cells[3].Value.ToString();
+                textBox3.Text = TabelProfi.Rows[idx.Index].Cells[2].Value.ToString();
+                textBox4.Text = TabelProfi.Rows[idx.Index].Cells[4].Value.ToString();
+                
+            }
+        }
+
+        private void TabelUniv_SelectionChanged(object sender, EventArgs e)
+        {
+
+
+            if (TabelUniv.SelectedRows.Count == 1)
+            {
+                var idx = TabelUniv.SelectedRows[0];
+                int idUniv = int.Parse(TabelUniv.Rows[idx.Index].Cells[0].Value.ToString());
+                textBox3.Text = idUniv.ToString();
+                using (SqlConnection con = new SqlConnection(connectionStr))
+                {
+                    try
+                    {
+                        con.Open();
+                        adapterProf = new SqlDataAdapter();
+
+                        string sql = "Select * from profesori where UniID = @IdUniv";
+                        SqlCommand comanda = new SqlCommand(sql, con);
+                        comanda.Parameters.AddWithValue("@IdUniv", idUniv);
+
+                        adapterProf.SelectCommand = comanda;
+                        ds.Tables["TabelProfi"].Clear();
+                        adapterProf.SelectCommand = comanda;
+                        adapterProf.Fill(ds, "TabelProfi");
+                        TabelProfi.DataSource = ds.Tables["TabelProfi"];
+                        con.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                using (SqlConnection con = new SqlConnection(connectionStr))
+                {
+                    try {
+                    con.Open();
+                    adapterProf.SelectCommand = new SqlCommand("Select * from Profesori", con);
+                    adapterProf.Fill(ds, "TabelProfi");
+                    TabelProfi.DataSource = ds.Tables["TabelProfi"];
+                    con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
+
